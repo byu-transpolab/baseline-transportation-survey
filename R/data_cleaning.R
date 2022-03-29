9e39622b589d630f5bce4e0938286713f8773287
Hayden Atchley <atchley.sha@gmail.com>
2022-03-29 14:58:55 -0600

    update targets script

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,23 +1,19 @@
-# library(tidyverse)
-# library(magrittr)
-
-################################################################################
-
-#Set parameters
-unneededcols <- expr(c(StartDate:Finished, LastName:Language))
-othercols <- c("mode", "complex", "city", "hs_modes", "gender", "parent_education")
-zoneorder <- c(5, 12, 6, 7, 9, 8, 10, 11, 4, 2) #order of zones in activity cols
-firstactcols <- expr(first_activity_5:first_activity_2)
-lastactcols <- expr(last_activity_5:last_activity_2)
-rankcols <- expr(rank_parking:rank_na)
-
-################################################################################
 
 #' Get the data out of the qualtrics file and clean it.
 #' 
 #' @param datapath Path to qualtrics csv output
+#' @param coordspath Path to coordinate reference file
 #' @param questionspath Path to annotated question labels file
-get_data <- function(datapath, questionspath){
+#' @param unneededcols Expression containing a vector of columns that are extraneous
+#' @param othercols Character vector of multiple choice questions that have an "other" text box
+#' @param zoneorder Numeric vector of the order of zones in the activity columns
+#' @param firstactcols Expression containing a vector of "first activity" column names
+#' @param lastactcols Expression containing a vector of "last activity" column names
+#' @param rankcols Expression containing a vector of column names for ranking reasons
+#' @param outputpath Path for writing cleaned data file
+clean_data <- function(datapath, coordspath, questionspath, unneededcols,
+                       othercols, zoneorder, firstactcols, lastactcols,
+                       rankcols, outputpath){
   
   #read in data and exclude unnecessary columns
   data <- read_csv(datapath) %>% 
@@ -41,104 +37,102 @@ get_data <- function(datapath, questionspath){
   data %<>% select(-(textcols+1))
   
   
+  #remove responses without a mode
+  data %<>% filter(!(is.na(mode) & is.na(mode_other)))
+  
+  #copy "other" text to main columns
+  textcols <- which(colnames(data) %in% othercols)
+  
+  #collapse "other" columns
+  for(i in textcols) data %<>% replace(i, coalesce(data[[i]], data[[i+1]]))
+  
+  #remove "other" columns
+  data %<>% select(-(textcols+1))
+  
+  
+  #reformat campus zone data
+  firstact_all <- data %>% 
+    select(!!firstactcols) %>% 
+    mutate(across(.fns = ~ replace(., . == "Like", "On")))
+  
+  lastact_all <- data %>% 
+    select(!!lastactcols) %>% 
+    mutate(across(.fns = ~ replace(., . == "Like", "On")))
+  
+  zoneorder <- zoneorder
+  
+  get_locations <- function(df){
+    #find which columns are "On"
+    cols <- which(df == "On", arr.ind = T) %>%
+      as_tibble() %>%
+      arrange(row) %>% 
+      select(col) %>% 
+      unlist() %>% 
+      unname()
+    
+    #use the column index to get the zone
+    zoneorder[cols]
+  }
+  
+  firstact <- get_locations(firstact_all)
+  lastact <- get_locations(lastact_all)
+  
+  #add new activity cols and remove old ones
+  data %<>% 
+    mutate(first_activity = firstact,
+           last_activity = lastact) %>% 
+    select(-c(!!firstactcols, !!lastactcols)) %>%
+    relocate(first_activity, last_activity, .before = school_year)
+  
+  
+  #reformat rankings for reasons
+  rankings <- data %>% 
+    select(ID, !!rankcols)
+  
+  rankings %<>% 
+    pivot_longer(-ID) %>% 
+    filter(!is.na(value)) %>% 
+    arrange(ID,value) %>% 
+    pivot_wider(ID, names_from = value, values_from = name) %>% 
+    #make the values look prettier
+    mutate(across(.fns = ~ gsub("rank_", "", .))) %>% 
+    mutate(across(-ID, ~ str_to_title(.)))
+  
+  #re-add rankings to table
+  data %<>%
+    left_join(rankings, by = "ID") %>% 
+    rename(rank_1 = `1`,
+           rank_2 = `2`,
+           rank_3 = `3`,
+           rank_4 = `4`,
+           rank_5 = `5`) %>% 
+    relocate(rank_1:rank_5, .after = reasons)
+  
+  #remove old ranking columns and reasons list
+  data %<>% select(-(!!rankcols), -reasons)
+  
+  
+  
+  #create coordinates of living location
+  coordslist <- read_csv(coordspath)
+  
+  #determine coords from pixels
+  coord_longitude <- 111.7083078-(as.numeric(data$coord_x)*0.0000241039393939354)
+  coord_latitude <- 40.2824881-(as.numeric(data$coord_y)*0.0000184697272727293)
+  
+  #join lats/longs based on above and predetermined values (coordslist)
+  data %<>% 
+    mutate(longitude = coord_longitude, latitude = coord_latitude) %>% 
+    left_join(coordslist, by = c("complex" = "location")) %>% 
+    left_join(coordslist, by = c("city" = "location")) %>% 
+    #coalesce all columns
+    mutate(longitude = coalesce(longitude, longitude.x, longitude.y),
+           latitude = coalesce(latitude, latitude.x, latitude.y)) %>% 
+    select(-c(coord_x:coord_y, longitude.x:latitude.y)) %>% 
+    relocate(longitude, latitude, .after = city)
+  
+  
+  data %>% write_csv(outputpath)
+  
   data
 }
-
-#remove responses without a mode
-data %<>% filter(!(is.na(mode) & is.na(mode_other)))
-
-#copy "other" text to main columns
-textcols <- which(colnames(data) %in% othercols)
-
-#collapse "other" columns
-for(i in textcols) data %<>% replace(i, coalesce(data[[i]], data[[i+1]]))
-
-#remove "other" columns
-data %<>% select(-(textcols+1))
-
-
-#reformat campus zone data
-firstact_all <- data %>% 
-  select(!!firstactcols) %>% 
-  mutate(across(.fns = ~ replace(., . == "Like", "On")))
-
-lastact_all <- data %>% 
-  select(!!lastactcols) %>% 
-  mutate(across(.fns = ~ replace(., . == "Like", "On")))
-
-zoneorder <- zoneorder
-
-get_locations <- function(df){
-  #find which columns are "On"
-  cols <- which(df == "On", arr.ind = T) %>%
-    as_tibble() %>%
-    arrange(row) %>% 
-    select(col) %>% 
-    unlist() %>% 
-    unname()
-  
-  #use the column index to get the zone
-  zoneorder[cols]
-}
-
-firstact <- get_locations(firstact_all)
-lastact <- get_locations(lastact_all)
-
-#add new activity cols and remove old ones
-data %<>% 
-  mutate(first_activity = firstact,
-         last_activity = lastact) %>% 
-  select(-c(!!firstactcols, !!lastactcols)) %>%
-  relocate(first_activity, last_activity, .before = school_year)
-
-
-#reformat rankings for reasons
-rankings <- data %>% 
-  select(ID, !!rankcols)
-
-rankings %<>% 
-  pivot_longer(-ID) %>% 
-  filter(!is.na(value)) %>% 
-  arrange(ID,value) %>% 
-  pivot_wider(ID, names_from = value, values_from = name) %>% 
-  #make the values look prettier
-  mutate(across(.fns = ~ gsub("rank_", "", .))) %>% 
-  mutate(across(-ID, ~ str_to_title(.)))
-
-#re-add rankings to table
-data %<>%
-  left_join(rankings, by = "ID") %>% 
-  rename(rank_1 = `1`,
-         rank_2 = `2`,
-         rank_3 = `3`,
-         rank_4 = `4`,
-         rank_5 = `5`) %>% 
-  relocate(rank_1:rank_5, .after = reasons)
-
-#remove old ranking columns and reasons list
-data %<>% select(-(!!rankcols), -reasons)
-
-
-
-#create coordinates of living location
-coordslist <- read_csv(coordspath)
-
-#determine coords from pixels
-coord_longitude <- 111.7083078-(as.numeric(data$coord_x)*0.0000241039393939354)
-coord_latitude <- 40.2824881-(as.numeric(data$coord_y)*0.0000184697272727293)
-
-#join lats/longs based on above and predetermined values (coordslist)
-data %<>% 
-  mutate(longitude = coord_longitude, latitude = coord_latitude) %>% 
-  left_join(coordslist, by = c("complex" = "location")) %>% 
-  left_join(coordslist, by = c("city" = "location")) %>% 
-  #coalesce all columns
-  mutate(longitude = coalesce(longitude, longitude.x, longitude.y),
-         latitude = coalesce(latitude, latitude.x, latitude.y)) %>% 
-  select(-c(coord_x:coord_y, longitude.x:latitude.y)) %>% 
-  relocate(longitude, latitude, .after = city)
-
-data
-
-data %>% write_csv(outputpath)
-
--- _targets.R
+++ _targets.R
@@ -1,41 +1,41 @@
-library(targets)
-library(tarchetypes)
-# This is an example _targets.R file. Every
-# {targets} pipeline needs one.
-# Use tar_script() to create _targets.R and tar_edit()
-# to open it again for editing.
-# Then, run tar_make() to run the pipeline
-# and tar_read(summary) to view the results.
-
-# Set target-specific options such as packages.
-tar_option_set(packages = c("tidyverse", "bookdown"))
-
-# Define custom functions and other global objects.
-# This is where you write source(\"R/functions.R\")
-# if you keep your functions in external scripts.
-source("R/functions.R")
-
-
-data_targets <- tar_plan(
-  data =  data.frame(x = sample.int(100), y = sample.int(100)),
-  summary = summ(data) # Call your custom functions as needed.
-)
-
-
-
-# Targets necessary to build the book / article
-book_targets <- tar_plan(
-  report = bookdown::render_book(input = ".", output_yaml = "_output.yml", 
-                                 config_file = "_bookdown.yml")
-)
-
-
-
-# run all targets
-tar_plan(
-  data = data_targets, 
-  book = book_targets
-)
+# library(targets)
+# library(tarchetypes)
+# # This is an example _targets.R file. Every
+# # {targets} pipeline needs one.
+# # Use tar_script() to create _targets.R and tar_edit()
+# # to open it again for editing.
+# # Then, run tar_make() to run the pipeline
+# # and tar_read(summary) to view the results.
+# 
+# # Set target-specific options such as packages.
+# tar_option_set(packages = c("tidyverse", "bookdown"))
+# 
+# # Define custom functions and other global objects.
+# # This is where you write source(\"R/functions.R\")
+# # if you keep your functions in external scripts.
+# source("R/functions.R")
+# 
+# 
+# data_targets <- tar_plan(
+#   data =  data.frame(x = sample.int(100), y = sample.int(100)),
+#   summary = summ(data) # Call your custom functions as needed.
+# )
+# 
+# 
+# 
+# # Targets necessary to build the book / article
+# book_targets <- tar_plan(
+#   report = bookdown::render_book(input = ".", output_yaml = "_output.yml", 
+#                                  config_file = "_bookdown.yml")
+# )
+# 
+# 
+# 
+# # run all targets
+# tar_plan(
+#   data = data_targets, 
+#   book = book_targets
+# )
 
 library(targets)
 # This is an example _targets.R file. Every
@@ -56,10 +56,21 @@ tar_option_set(packages = c("tidyverse", "magrittr"))
 
 # End this file with a list of target objects.
 list(
-  tar_target(datapath, "data/test_data.csv", format = "file"),
+  ### Set parameters ###
+  tar_target(datapath, "data/test_data_monday.csv", format = "file"),
   tar_target(coordspath, "reference/coords_list.csv", format = "file"),
-  tar_target(questionspath, "reference/question_names.csv", format = "file"),
-  tar_target(data, get_data(datapath, questionspath))
+  tar_target(questionspath, "reference/question_names.txt", format = "file"),
+  tar_target(unneededcols, expr(c(StartDate:Finished, LastName:Language))),
+  tar_target(othercols, c("mode", "complex", "city", "hs_modes", "gender", "parent_education")),
+  tar_target(zoneorder, c(5, 12, 6, 7, 9, 8, 10, 11, 4, 2)), #order of zones in activity cols
+  tar_target(firstactcols, expr(first_activity_5:first_activity_2)),
+  tar_target(lastactcols, expr(last_activity_5:last_activity_2)),
+  tar_target(rankcols, expr(rank_parking:rank_na)),
+  tar_target(outputpath, "data/test_data_monday_CLEANED.csv"),
   
+  ### Clean data ###
+  tar_target(data, clean_data(datapath, coordspath, questionspath, unneededcols,
+                              othercols, zoneorder, firstactcols, lastactcols,
+                              rankcols, outputpath))
   
 )
